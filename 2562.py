7cbdd9c41b86a9549e1de4025f78812a5fe6e418
Harry_Yoon <haesoo9410@gmail.com>
2020-10-19 22:30:39 +0900

    2562

---
+++ 2562.py
@@ -0,0 +1,11 @@
+lst = list()
+for _ in range(9) :
+    lst.append(int(input()))
+max_n = lst[0]
+max_i = 0
+for i in range(1, 9) :
+    if max_n < lst[i] :
+        max_n = lst[i]
+        max_i = i+1
+print(max_n)
+print(i)
