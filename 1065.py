aab0b3fe3c8d2ba2e6e65293a783794180b9e212
Harry_Yoon <haesoo9410@gmail.com>
2020-10-30 20:33:18 +0900

    1065

--- 1065.py
+++ 1065.py
@@ -1,27 +1,30 @@
-N = int(input())
-if N < 100 :
-    count = N
-elif N < 1000 :
-    count = 99
-    for i in range(100,N+1) :
-        hh = i // 100
-        tt = (i % 100) // 10
-        dd = (i % 100) % 10
-        if hh-tt == tt-dd :
-            count += 1
-        else :
-            pass
-elif N == 1000 :
-    count = 99
-    for i in range(100,N) :
-        hh = i // 100
-        tt = (i % 100) // 10
-        dd = (i % 100) % 10
-        if hh-tt == tt-dd :
-            count += 1
-        else :
-            pass
+def solve(N) :
+    if N < 100:
+        count = N
+    elif N < 1000:
+        count = 99
+        for i in range(100, N + 1):
+            hh = i // 100
+            tt = (i % 100) // 10
+            dd = (i % 100) % 10
+            if hh - tt == tt - dd:
+                count += 1
+            else:
+                pass
+    elif N == 1000:
+        count = 99
+        for i in range(100, N):
+            hh = i // 100
+            tt = (i % 100) // 10
+            dd = (i % 100) % 10
+            if hh - tt == tt - dd:
+                count += 1
+            else:
+                pass
+    print(count)
+
+n = int(input())
+solve(n)
 
 
-print(count)
 
