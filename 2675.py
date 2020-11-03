30f24dd13931b70b6ce338bdb4a487c5716f88e0
Harry_Yoon <haesoo9410@gmail.com>
2020-11-03 20:43:11 +0900

    2675(2)

--- 2675.py
+++ 2675.py
@@ -1,3 +1,6 @@
-R, S = input().split()
-for i in S :
-    print(i*int(R),end="")
+T = int(input())
+for i in range(T) :
+    R, S = input().split()
+    for i in S :
+        print(i*int(R),end="")
+    print()
