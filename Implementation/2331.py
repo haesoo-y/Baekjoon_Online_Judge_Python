7a4d39b81af1b3275ec7ca7994563009688dc5fc
Harry_Yoon <haesoo9410@gmail.com>
2021-03-15 22:22:20 +0900

    Add 2331.py

---
+++ Implementation/2331.py
@@ -0,0 +1,17 @@
+def multiply_p(x) :
+    x = int(x) ** p
+    return x
+
+a, p = map(int, input().split())
+lst = [a]
+i = 0
+# lst.append(0)
+# lst[1] = sum(map(multiply_p, str(lst[0])))
+while True :
+    i += 1
+    num = sum(map(multiply_p, str(lst[i-1])))
+    if num in lst :
+        break
+    lst.append(num)
+
+print(lst.index(num))
